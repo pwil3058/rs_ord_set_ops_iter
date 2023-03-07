a6648d1e1b9741828cdadc7208144527074c02f8
Peter Williams <pwil3058@bigpond.net.au>
2023-03-07 22:20:52 +1000

    Star benchmarking

--- ord_list_set/benches/benchmark.rs
+++ ord_list_set/benches/benchmark.rs
@@ -21,6 +21,25 @@ pub fn from_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
+pub fn iter_benchmark(c: &mut Criterion) {
+    let data = ["a", "b", "c", "g", "e", "f"];
+    let btree_set = BTreeSet::from(data);
+    let ord_list_set = OrdListSet::from(data);
+
+    let mut group = c.benchmark_group("iter().collect(): OrdListSet vs BTreeSet");
+    group.bench_function("BTreeSet", |b| {
+        b.iter(|| {
+            let _result = btree_set.iter().collect::<Vec<_>>();
+        })
+    });
+    group.bench_function("OrdListSet", |b| {
+        b.iter(|| {
+            let _result = ord_list_set.iter().collect::<Vec<_>>();
+        })
+    });
+    group.finish();
+}
+
 pub fn union_benchmark(c: &mut Criterion) {
     let data1 = ["a", "b", "c", "g", "e", "f"];
     let data2 = ["c", "f", "i", "l"];
@@ -30,8 +49,9 @@ pub fn union_benchmark(c: &mut Criterion) {
     let ord_list_set2 = OrdListSet::from(data2);
 
     let mut group = c.benchmark_group("Union: OrdListSet vs BTreeSet");
-    group.bench_function("BTreeSet: .union().cloned()", |b| {
+    group.bench_function("BTreeSet: .union().collect()", |b| {
         b.iter(|| {
+            //let _result: BTreeSet<&str> = btree_set1.union(&btree_set2).cloned().collect();
             let _result = BTreeSet::<&str>::from_iter(btree_set1.union(&btree_set2).cloned());
         })
     });
@@ -40,10 +60,9 @@ pub fn union_benchmark(c: &mut Criterion) {
             let _result: BTreeSet<&str> = &btree_set1 | &btree_set2;
         })
     });
-    group.bench_function("OrdListSet: .union().cloned()", |b| {
+    group.bench_function("OrdListSet: .union().collect()", |b| {
         b.iter(|| {
-            let _result =
-                OrdListSet::<&str>::from_iter(ord_list_set1.union(&ord_list_set2).cloned());
+            let _result: OrdListSet<&str> = ord_list_set1.union(&ord_list_set2).into();
         })
     });
     group.bench_function("OrdListSet: '|' operator'", |b| {
@@ -54,5 +73,5 @@ pub fn union_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, from_benchmark, union_benchmark,);
+criterion_group!(benches, from_benchmark, iter_benchmark, union_benchmark,);
 criterion_main!(benches);
