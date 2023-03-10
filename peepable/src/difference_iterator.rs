d83f64dd722663a82f06e9eb4df03c4a9409a53a
Peter Williams <pwil3058@bigpond.net.au>
2023-03-10 16:15:36 +1000

    New experimental implementation: peepable

---
+++ peepable/src/difference_iterator.rs
@@ -0,0 +1,111 @@
+// Copyright 2023 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
+
+use std::cmp::Ordering;
+
+use super::{OrdSetIterSetOpsIterator, PeepAdvanceIter};
+
+#[macro_export]
+macro_rules! difference_next {
+    ($left_iter: expr, $right_iter: expr) => {{
+        loop {
+            if let Some(l_item) = $left_iter.peep() {
+                if let Some(r_item) = $right_iter.peep() {
+                    match l_item.cmp(r_item) {
+                        Ordering::Less => {
+                            break $left_iter.next();
+                        }
+                        Ordering::Greater => {
+                            $right_iter.advance_until(l_item);
+                        }
+                        Ordering::Equal => {
+                            $left_iter.next();
+                            $right_iter.next();
+                        }
+                    }
+                } else {
+                    break $left_iter.next();
+                }
+            } else {
+                break None;
+            }
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! difference_peep {
+    ($left_iter: expr, $right_iter: expr) => {{
+        loop {
+            if let Some(l_item) = $left_iter.peep() {
+                if let Some(r_item) = $right_iter.peep() {
+                    match l_item.cmp(r_item) {
+                        Ordering::Less => {
+                            break Some(l_item);
+                        }
+                        Ordering::Greater => {
+                            $right_iter.advance_until(l_item);
+                        }
+                        Ordering::Equal => {
+                            $left_iter.next();
+                            $right_iter.next();
+                        }
+                    }
+                } else {
+                    break Some(l_item);
+                }
+            } else {
+                break None;
+            }
+        }
+    }};
+}
+
+#[derive(Clone)]
+pub struct DifferenceIterator<'a, T: Ord + Clone> {
+    left_iter: Box<dyn PeepAdvanceIter<'a, T> + 'a>,
+    right_iter: Box<dyn PeepAdvanceIter<'a, T> + 'a>,
+}
+
+impl<'a, T: Ord + Clone> DifferenceIterator<'a, T> {
+    pub fn new(
+        left_iter: impl PeepAdvanceIter<'a, T> + 'a,
+        right_iter: impl PeepAdvanceIter<'a, T> + 'a,
+    ) -> Self {
+        Self {
+            left_iter: Box::new(left_iter),
+            right_iter: Box::new(right_iter),
+        }
+    }
+}
+
+impl<'a, T: Ord + Clone> Iterator for DifferenceIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        difference_next!(self.left_iter, self.right_iter)
+    }
+}
+
+impl<'a, T> PeepAdvanceIter<'a, T> for DifferenceIterator<'a, T>
+where
+    T: 'a + Ord + Clone,
+{
+    fn peep(&mut self) -> Option<&'a T> {
+        difference_peep!(self.left_iter, self.right_iter)
+    }
+
+    fn advance_until(&mut self, target: &T) {
+        self.left_iter.advance_until(target);
+        self.right_iter.advance_until(target);
+    }
+
+    fn advance_after(&mut self, target: &T) {
+        self.left_iter.advance_after(target);
+        self.right_iter.advance_after(target);
+    }
+}
+
+impl<'a, T: 'a + Ord + Clone + Default> OrdSetIterSetOpsIterator<'a, T>
+    for DifferenceIterator<'a, T>
+{
+}
---
+++ peepable/src/intersection_iterator.rs
@@ -0,0 +1,108 @@
+// Copyright 2023 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
+
+use std::cmp::Ordering;
+
+use super::{OrdSetIterSetOpsIterator, PeepAdvanceIter};
+
+#[macro_export]
+macro_rules! intersection_next {
+    ($left_iter: expr, $right_iter: expr) => {{
+        loop {
+            if let Some(l_item) = $left_iter.peep() {
+                if let Some(r_item) = $right_iter.peep() {
+                    match l_item.cmp(r_item) {
+                        Ordering::Less => {
+                            $left_iter.advance_until(r_item);
+                        }
+                        Ordering::Greater => {
+                            $right_iter.advance_until(l_item);
+                        }
+                        Ordering::Equal => {
+                            $right_iter.next();
+                            break $left_iter.next();
+                        }
+                    }
+                } else {
+                    break None;
+                }
+            } else {
+                break None;
+            }
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! intersection_peep {
+    ($left_iter: expr, $right_iter: expr) => {{
+        loop {
+            if let Some(l_item) = $left_iter.peep() {
+                if let Some(r_item) = $right_iter.peep() {
+                    match l_item.cmp(r_item) {
+                        Ordering::Less => {
+                            $left_iter.advance_until(r_item);
+                        }
+                        Ordering::Greater => {
+                            $right_iter.advance_until(l_item);
+                        }
+                        Ordering::Equal => break Some(l_item),
+                    }
+                } else {
+                    break None;
+                }
+            } else {
+                break None;
+            }
+        }
+    }};
+}
+
+#[derive(Clone)]
+pub struct IntersectionIterator<'a, T: Ord + Clone> {
+    left_iter: Box<dyn PeepAdvanceIter<'a, T> + 'a>,
+    right_iter: Box<dyn PeepAdvanceIter<'a, T> + 'a>,
+}
+
+impl<'a, T: Ord + Clone> IntersectionIterator<'a, T> {
+    pub fn new(
+        left_iter: impl PeepAdvanceIter<'a, T> + 'a,
+        right_iter: impl PeepAdvanceIter<'a, T> + 'a,
+    ) -> Self {
+        Self {
+            left_iter: Box::new(left_iter),
+            right_iter: Box::new(right_iter),
+        }
+    }
+}
+
+impl<'a, T: Ord + Clone> Iterator for IntersectionIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        intersection_next!(self.left_iter, self.right_iter)
+    }
+}
+
+impl<'a, T> PeepAdvanceIter<'a, T> for IntersectionIterator<'a, T>
+where
+    T: 'a + Ord + Clone,
+{
+    fn peep(&mut self) -> Option<&'a T> {
+        intersection_peep!(self.left_iter, self.right_iter)
+    }
+
+    fn advance_until(&mut self, target: &T) {
+        self.left_iter.advance_until(target);
+        self.right_iter.advance_until(target);
+    }
+
+    fn advance_after(&mut self, target: &T) {
+        self.left_iter.advance_after(target);
+        self.right_iter.advance_after(target);
+    }
+}
+
+impl<'a, T: 'a + Ord + Clone + Default> OrdSetIterSetOpsIterator<'a, T>
+    for IntersectionIterator<'a, T>
+{
+}
